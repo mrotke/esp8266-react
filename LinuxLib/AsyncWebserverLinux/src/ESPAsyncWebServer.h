e6014a8ed02a09de89ab9666b7443e3977770662
microt <microt@mrotke.cba.pl>
2022-06-02 21:02:12 +0200

    WIP: Compliation on linux

--- LinuxLib/AsyncWebserverLinux/src/ESPAsyncWebServer.h
+++ LinuxLib/AsyncWebserverLinux/src/ESPAsyncWebServer.h
@@ -3,13 +3,25 @@
 
 #include <functional>
 #include <ArduinoJson.h>
+#include <httpserver.hpp>
 
-class AsyncWebServer {
-};
+using namespace httpserver;
+
+typedef enum {
+  HTTP_GET     = 0b00000001,
+  HTTP_POST    = 0b00000010,
+  HTTP_DELETE  = 0b00000100,
+  HTTP_PUT     = 0b00001000,
+  HTTP_PATCH   = 0b00010000,
+  HTTP_HEAD    = 0b00100000,
+  HTTP_OPTIONS = 0b01000000,
+  HTTP_ANY     = 0b01111111,
+} WebRequestMethod;
+
+typedef uint8_t WebRequestMethodComposite;
 
 class AsyncWebServerRequest {
 };
-
 /*
  * FILTER :: Callback to filter AsyncWebRewrite and AsyncWebHandler (done by the Server)
  * */
@@ -17,5 +29,16 @@ class AsyncWebServerRequest {
 typedef std::function<bool(AsyncWebServerRequest *request)> ArRequestFilterFunction;
 typedef std::function<void(AsyncWebServerRequest *request, JsonVariant &json)> ArJsonRequestHandlerFunction;
 typedef std::function<void(AsyncWebServerRequest *request)> ArRequestHandlerFunction;
+
+class AsyncWebServer {
+public:
+    AsyncWebServer();
+    AsyncWebServer(uint16_t port);
+    on(const char* uri, WebRequestMethodComposite method, ArRequestHandlerFunction onRequest);
+private:
+    webserver ws;
+};
+
+
 
 #endif
