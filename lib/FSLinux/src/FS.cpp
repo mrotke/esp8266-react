459647925d795b31945ad7e51fec4ca5d95aeea9
microt <microt@mrotke.cba.pl>
2022-08-07 14:39:44 +0200

    Fix file system for linux

--- lib/FSLinux/src/FS.cpp
+++ lib/FSLinux/src/FS.cpp
@@ -1,5 +1,6 @@
 #include "FS.h"
 #include <string.h>
+#include <algorithm>
 
 using namespace std;
 
@@ -7,11 +8,24 @@ FS linuxFS = FS();
 
 File FS::open(const char* path, const char* mode)
 {
+    string flatPath = changePath2Flat(path);
     fstream file_Handler;
+    
+
+    file_Handler.open(flatPath, ios::out | ios::app);
+    file_Handler.close();
+
     if (strcmp(mode,"r") == 0)
-        file_Handler.open(path, ios::in);
+        file_Handler.open(flatPath, ios::in);
     if (strcmp(mode,"w") == 0)
-        file_Handler.open(path, ios::out);
+        file_Handler.open(flatPath, ios::out);
 
     return file_Handler;
+}
+
+string FS::changePath2Flat(const char* path)
+{
+    string str(path);
+    std::replace(str.begin(),str.end(),'/','_');
+    return str;
 }
--- lib/FSLinux/src/FS.h
+++ lib/FSLinux/src/FS.h
@@ -10,6 +10,8 @@ class FS
 {
 public:
 File open(const char* path, const char* mode);
+private:
+std::string changePath2Flat(const char* path);
 };
 
 extern FS linuxFS;
